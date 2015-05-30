ad46d6d6a373378901dfbe5b0b1ec2e60a5b2226
volodg <gorbenko.vova@gmail.com>
2015-05-30 21:33:47 +0300

    initial commits

--- Pods/Target Support Files/Pods-iAsync-iAsync.async/Pods-iAsync-iAsync.async-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iAsync_iAsync_async : NSObject
-@end
-@implementation PodsDummy_Pods_iAsync_iAsync_async
-@end
--- Pods/Target Support Files/Pods-iAsync/Pods-iAsync-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iAsync : NSObject
-@end
-@implementation PodsDummy_Pods_iAsync
-@end
