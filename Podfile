a0a283f1f77bd5cc422bf7aefaea04ee5e48ada3
volodg <gorbenko.vova@gmail.com>
2015-05-30 21:33:25 +0300

    initial commits

--- Podfile
+++ Podfile
@@ -1,7 +1,7 @@
 platform :ios, '8.0'
 use_frameworks!
 
-target 'iAsync' do
+target 'iAsync.async' do
 
 pod 'iAsync.async', '~> 0.0.1'
 #pod 'iAsync.async', :path => "."
---
+++ Pods/Target Support Files/Pods-iAsync.async-iAsync.async/Pods-iAsync.async-iAsync.async-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iAsync_async_iAsync_async : NSObject
+@end
+@implementation PodsDummy_Pods_iAsync_async_iAsync_async
+@end
