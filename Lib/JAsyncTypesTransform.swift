f99158ba66f92d65fda71e759a3787d5fa6457fb
volodg <volodg@mail.ru>
2015-06-25 23:10:10 +0300

    Merge remote-tracking branch 'origin/master' into swift.2
    # Conflicts:
    #	Lib/JAsyncContinuity.swift

--- Lib/JAsyncTypesTransform.swift
+++ Lib/JAsyncTypesTransform.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+import iAsync_async
+
 public enum JAsyncTypesTransform<T1, T2> {
     
     private typealias Async1 = JAsyncTypes<T1>.JAsync
@@ -18,4 +20,26 @@ public enum JAsyncTypesTransform<T1, T2> {
     private typealias PackedAsync = JAsyncTypes<PackedType>.JAsync
     
     public typealias AsyncTransformer = (PackedAsync) -> PackedAsync
+    
+    public static func transformLoadersType1(async1: Async1, transformer: AsyncTransformer) -> Async1 {
+        
+        let packedLoader = bindSequenceOfAsyncs(async1, { result -> PackedAsync in
+            return async(result: (result, nil))
+        })
+        let transformedLoader = transformer(packedLoader)
+        return bindSequenceOfAsyncs(transformedLoader, { result -> Async1 in
+            return async(result: result.0!)
+        })
+    }
+    
+    public static func transformLoadersType2(async2: Async2, transformer: AsyncTransformer) -> Async2 {
+        
+        let packedLoader = bindSequenceOfAsyncs(async2, { result -> PackedAsync in
+            return async(result: (nil, result))
+        })
+        let transformedLoader = transformer(packedLoader)
+        return bindSequenceOfAsyncs(transformedLoader, { result -> Async2 in
+            return async(result: result.1!)
+        })
+    }
 }
